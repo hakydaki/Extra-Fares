c1ca06346789ee19ee0670f5140af3d913e24465
Haki <speefy49@gmail.com>
2026-05-24 04:12:50 -0700

    started block registry
    what am i doing

--- src/main/java/net/hakydaki/extrafares/content/ExtraFaresBlocks.java
+++ src/main/java/net/hakydaki/extrafares/content/ExtraFaresBlocks.java
@@ -1,10 +1,18 @@
 package net.hakydaki.extrafares.content;
 
-import net.hecco.bountifulfares.definition.block.custom.FruitLogBlock;
+import net.hecco.bountifulfares.BountifulFares;
+import net.hecco.bountifulfares.definition.block.custom.*;
 import net.hecco.bountifulfares.registry.content.BFBlocks;
 
+import net.hecco.bountifulfares.registry.misc.BFSaplingGenerators;
+import net.hecco.nexuslib.lib.publicBlocks.PublicSaplingBlock;
+import net.hecco.nexuslib.platform.NLServices;
 import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.state.properties.NoteBlockInstrument;
+import net.minecraft.world.level.material.MapColor;
+import net.minecraft.world.level.material.PushReaction;
 
 import java.util.function.Supplier;
 
@@ -13,6 +21,32 @@ import static net.hecco.bountifulfares.registry.content.BFBlocks.registerBlock;
 
 public class ExtraFaresBlocks {
 
-    //Tree woods
+    //Tree logs
     public static final Supplier<Block> LIME_LOG = registerBlock("lime_log", () -> new FruitLogBlock(BlockBehaviour.Properties.ofFullCopy(BFBlocks.APPLE_LOG.get())));
+    //Tree planks
+    public static final Supplier<Block> LIME_WOOD = registerBlock("Lime_wood", () -> new FruitLogBlock(BlockBehaviour.Properties.ofFullCopy(BFBlocks.APPLE_WOOD.get())));
+    //Tree stripped logs
+    public static final Supplier<Block> STRIPPED_LIME_LOG = registerBlock("stripped_Lime_log", () -> new StrippedFruitLogBlock(BlockBehaviour.Properties.ofFullCopy(BFBlocks.STRIPPED_APPLE_LOG.get())));
+    //Tree stripped planks
+    public static final Supplier<Block> STRIPPED_Lime_WOOD = registerBlock("stripped_lime_wood", () -> new StrippedFruitLogBlock(BlockBehaviour.Properties.ofFullCopy(BFBlocks.STRIPPED_APPLE_WOOD.get())));
+    //Hanging fruits
+    public static final Supplier<Block> HANGING_LIME = registerBlockNoItem("hanging_lime", () -> new HangingLemonBlock(BlockBehaviour.Properties.ofFullCopy(BFBlocks.HANGING_APPLE.get())));
+    //Fruit leaves
+    public static final Supplier<Block> LIME_LEAVES = registerBlock("lime_leaves", () -> new FruitLeavesBlock(HANGING_LIME.get().defaultBlockState(), BlockBehaviour.Properties.ofFullCopy(BFBlocks.APPLE_LEAVES.get())));
+    //Flowering fruit leaves
+    public static final Supplier<Block> FLOWERING_LIME_LEAVES = registerBlock("flowering_lime_leaves", () -> new FruitLeavesBlock(HANGING_LIME.get().defaultBlockState(), BlockBehaviour.Properties.ofFullCopy(BFBlocks.FLOWERING_APPLE_LEAVES.get())));
+    //Tree saplings
+    public static final Supplier<Block> LIME_SAPLING = registerBlock("lime_sapling", () -> new PublicSaplingBlock(BFSaplingGenerators.LEMON_SAPLING_GENERATOR, BlockBehaviour.Properties.ofFullCopy(BFBlocks.APPLE_SAPLING.get())));
+    //Potted saplings
+    public static final Supplier<Block> POTTED_LIME_SAPLING = registerBlockNoItem("potted_lime_sapling", () -> new FlowerPotBlock(BFBlocks.LEMON_SAPLING.get(), BlockBehaviour.Properties.ofFullCopy(BFBlocks.POTTED_APPLE_SAPLING.get())));
+    //Fruit blocks
+    public static final Supplier<Block> LIME_BLOCK = registerBlock("lime_block", () -> new LemonBlock(BlockBehaviour.Properties.of().mapColor(MapColor.COLOR_LIGHT_GREEN).strength(0.5f).instrument(NoteBlockInstrument.DIDGERIDOO).sound(SoundType.WOOD)));
+    //Pies
+    public static final Supplier<Block> LIME_PIE = registerBlock("lime_pie", () -> new PieBlock(BlockBehaviour.Properties.of().noOcclusion().forceSolidOn().strength(0.5F).sound(SoundType.WOOL).pushReaction(PushReaction.DESTROY)), new Item.Properties().stacksTo(16));
+
+    //helper methods
+    private static Supplier<Block> registerBlockNoItem(String name, Supplier<Block> block) {
+        return NLServices.REGISTRY.registerBlockNoItem(BountifulFares.MOD_ID, name, block);
+    }
+    }
 }
---
+++ src/main/java/net/hakydaki/extrafares/content/ExtraFaresItems.java
@@ -0,0 +1,35 @@
+package net.hakydaki.extrafares.content;
+
+import net.hecco.bountifulfares.BountifulFares;
+import net.hecco.bountifulfares.definition.item.custom.*;
+import net.hecco.bountifulfares.registry.content.BFEffects;
+import net.hecco.bountifulfares.registry.content.BFItems;
+import net.hecco.nexuslib.platform.NLServices;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.food.FoodProperties;
+import net.minecraft.world.item.*;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.level.block.Blocks;
+
+import java.util.List;
+import java.util.function.Supplier;
+
+import static net.hecco.bountifulfares.registry.content.BFItems.CUP;
+
+
+public class ExtraFaresItems {
+
+    //Fruit Items
+    public static final Supplier<Item> LIME = registerItem("lime", () -> new Item(new Item.Properties().food(new FoodProperties.Builder().nutrition(4).saturationModifier(0.3F).build())));
+    //Candied Fruits
+    public static final Supplier<Item> CANDIED_LIME = registerItem("candied_lime", () -> new Item(new Item.Properties().food(new FoodProperties.Builder().nutrition(2).saturationModifier(0.2f).fast().build())));
+    //Jars
+    public static final Supplier<Item> LIME_COMPOTE_JAR = registerItem("lime_compote_jar", () -> new EdibleJarItem(List.of(new MobEffectInstance(BFEffects.ENRICHMENT, 1200, 0)), SoundEvents.HONEY_DRINK, new Item.Properties().stacksTo(16).craftRemainder(CUP.get()).food(new FoodProperties.Builder().nutrition(6).saturationModifier(0.4f).effect(new MobEffectInstance(BFEffects.ENRICHMENT, 1200, 0), 1f).build()).craftRemainder(CUP.get())));
+
+    //helper methods
+    private static Supplier<Item> registerItem(String id, Supplier<Item> registry) {
+        return NLServices.REGISTRY.register(BountifulFares.MOD_ID, id, BuiltInRegistries.ITEM, registry);
+    }
+}
